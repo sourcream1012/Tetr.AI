be513c12daaf2ea03df5c93c6b4f65fba9b36d1b
Caleb Welch <caleb.grapejuice@gmail.com>
2026-09-16 22:12:39 -0400

    feat: Input the name of the model you want to train on train.py startup

--- train.py
+++ train.py
@@ -3,14 +3,14 @@ import src.ai.trainer as trainer
 import src.tetris.game as game
 import threading
 
+name = input("What is the name of the AI you want to train > ")
+
 Tetris = game.Tetris()
-AI_Trainer = trainer.Trainer(Tetris)
-
+model = trainer.Trainer(Tetris)
 def start_env():
-    Tetris.start_ai_env(True, False, 0)
+    Tetris.start_ai_env(True, False)
 
-thread_start_env = threading.Thread(target=start_env)
-thread_start_env.start()
+thread = threading.Thread(target=start_env)
+thread.start()
 
-AI_Trainer.train("Tetr.AI")
-    
+model.train(name)
